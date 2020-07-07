71b86dd788818d629cf4c9823110000ae8aafa3c
sumukhbhat2701 <sumukhbhat2701@gmail.com>
2020-07-07 21:06:01 +0530

    File upload

--- routes/books.js
+++ routes/books.js
@@ -1,19 +1,10 @@
 const express=require('express')
 const router=express.Router()
-const multer= require('multer')
-const path= require('path')
 const Book=require('../models/book')
 const Author = require('../models/author')
-const fs=require('fs')
-const uploadPath = path.join('public',Book.coverImageBasePath)
+
 const imageMimeTypes = ['image/jpeg','image/png','image/gif','image/jpg']
 
-const upload = multer({
-    dest:uploadPath,
-    fileFilter: (req,file,callback) =>{
-        callback(null,imageMimeTypes.includes(file.mimetype))
-    }
-})
 
 //All books route->index.ejs in books folder
 router.get('/',async (req,res) => {
@@ -47,25 +38,21 @@ router.get('/new',async (req,res) => {
 })
 
 //Create new book route->no new file required
-router.post('/',upload.single('cover'),async (req,res)=>{  
-    const fileName = req.file!=null ? req.file.filename : null 
+router.post('/',async (req,res)=>{  
     const book = new Book({
         title:req.body.title,
         author:req.body.author,
         publishDate:new Date(req.body.publishDate),
         pageCount:req.body.pageCount,
-        coverImageName:fileName,
         description:req.body.description
     })
+    saveCover(book,req.body.cover)
     
     try {
         const newBook = await book.save()
         //res.redirect('books/${newBook.id}')
         res.redirect('books')
     } catch {
-        if(book.coverImageName!=null){
-            removeBookCover(book.coverImageName)
-        }
         renderNewPage(res,book,true)
     }
 })
@@ -88,10 +75,13 @@ async function renderNewPage(res,book,hasError=false){
     }
 }
 
-function removeBookCover(filename){
-	fs.unlink(path.join(uploadPath,filename),err=>{  
-		if(err) console.error(err)
-	})
+function saveCover(book,coverEncoded){
+    if(coverEncoded == null) return
+    const cover = JSON.parse(coverEncoded)
+    if(cover!=null && imageMimeTypes.includes(cover.type)){
+        book.coverImage=new Buffer.from(cover.data,'base64')
+        book.coverImageType=cover.type
+    }
 }
 
 module.exports=router
